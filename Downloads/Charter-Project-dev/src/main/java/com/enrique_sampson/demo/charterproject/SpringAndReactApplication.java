14d2e0bf7e91d43404544a16cfa859480c47e8cb
leanardos1212 <67398132+leanardos1212@users.noreply.github.com>
2021-04-13 12:22:13 -0500

    Update SpringAndReactApplication.java

--- Downloads/Charter-Project-dev/src/main/java/com/enrique_sampson/demo/charterproject/SpringAndReactApplication.java
+++ Downloads/Charter-Project-dev/src/main/java/com/enrique_sampson/demo/charterproject/SpringAndReactApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringAndReactApplication {
+public class CharterProjecttApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringAndReactApplication.class, args);
